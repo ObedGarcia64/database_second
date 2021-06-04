c2772f000910de0b115f56e9d3274b6a8568ad50
ObedGarcia64 <a1119150014@utch.edu.mx>
2021-06-03 23:25:44 -0600

    fixing errors and adding to the gitignore

--- db_config.py
+++ db_config.py
@@ -5,10 +5,12 @@ import os
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DBNAME")
+DB_NAME = os.getenv("DB_NAME")
 
 
 client = pymongo.MongoClient(
-    f"mongodb+srv://{DB_USER}:<{DB_PASSWORD}>@example.vfkmc.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+    f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@example.vfkmc.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
 
-db = client.Badges
+
+#Here you need to add the database name
+db = client.db_example
--- main.py
+++ main.py
@@ -6,18 +6,32 @@ import db_config as database
 app=Flask(__name__)
 api=Api(app)
 
+"""
+
+database.db.Badges.
+
+.database is the config file
+.db is the database name
+.NameCollection The next part is the collection
+
+"""
+
+class Test(Resource):
+    def get(self):
+        return jsonify({"message":"Test ok, you're connect"})
+
 class Badge(Resource):
     def post(self):
         database.db.Badges.insert_one(
             {
                 'header_img_url':request.json['header_img_url'],
-                'profile_picture_url':reques.json['profile_picture_url'],
-                'name':reques.json['name'],
-                'age':reques.json['age'],
-                'city':reques.json['city'],
-                'followers':reques.json['followers'],
-                'likes':reques.json['likes'],
-                'post':reques.json['post'],
+                'profile_picture_url':request.json['profile_picture_url'],
+                'name':request.json['name'],
+                'age':request.json['age'],
+                'city':request.json['city'],
+                'followers':request.json['followers'],
+                'likes':request.json['likes'],
+                'post':request.json['post'],
             }
         )
 
@@ -26,7 +40,8 @@ class AllBadge(Resource):
     def get(self):
         pass
 
-api. add_resource(Badge,'/new')
+api. add_resource(Badge,'/new/')
+api. add_resource(Test,'/test/')
 
 if __name__ == '__main__':
     app.run(load_dotenv=True)
